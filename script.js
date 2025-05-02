1cea8b99dc6098237f8e61812c1546394e7ac177
Yoori Choi <yoori336@gmail.com>
2025-05-02 19:36:40 +0900

    second

--- script.js
+++ script.js
@@ -30,6 +30,16 @@ const formEvent = (e) => {
   localStorage.setItem(`user_${userId}`, JSON.stringify(userData));
   alert(`${userId}님, 회원가입이 완료되었습니다!`);
 
+  gtag("config", "G-HRRBBCXT49", {
+    user_id: userId,
+    debug_mode: true,
+  });
+
+  gtag("event", "sign_up", {
+    user_id: userId,
+    method: "local_form",
+  });
+
   form.reset();
 };
 
